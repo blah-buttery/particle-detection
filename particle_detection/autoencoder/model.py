adda006c197c268dfedc9df21fa116fd33ef18c9
blah-buttery <toddyleelee@gmail.com>
2025-02-15 03:22:39 -0500

    replace first layer to handle grayscale image

--- particle_detection/autoencoder/model.py
+++ particle_detection/autoencoder/model.py
@@ -26,7 +26,33 @@ class Autoencoder(nn.Module):
         x = self.encoder(x)
         x = x.view(x.size(0), -1)  # Flatten for clustering
         return x
+        
+def create_autoencoder():
+    """
+    Creates an Autoencoder with a pretrained VGG16 encoder, modified for grayscale images.
+    """
+    pretrained_encoder = models.vgg16(weights="IMAGENET1K_V1")
 
+    # Modify first conv layer to accept grayscale (1-channel input)
+    new_first_layer = nn.Conv2d(
+        in_channels=1,  # Change from 3 to 1
+        out_channels=64,
+        kernel_size=3,
+        stride=1,
+        padding=1
+    )
+
+    # Copy weights (average RGB channels to single grayscale channel)
+    new_first_layer.weight.data = pretrained_encoder.features[0].weight.mean(dim=1, keepdim=True)
+
+    # Replace the first layer in the encoder
+    pretrained_encoder.features[0] = new_first_layer
+
+    # Remove last layer (classifier part)
+    encoder = nn.Sequential(*list(pretrained_encoder.features.children())[:-1])
+
+    return Autoencoder(encoder)
+'''
 def create_autoencoder():
     """
     Creates an Autoencoder with a pretrained VGG16 encoder.
@@ -34,3 +60,4 @@ def create_autoencoder():
     pretrained_encoder = models.vgg16(weights="IMAGENET1K_V1")
     encoder = nn.Sequential(*list(pretrained_encoder.features.children())[:-1])
     return Autoencoder(encoder)
+'''
